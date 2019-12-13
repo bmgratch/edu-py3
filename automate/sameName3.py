edafbd378d44f7ba61548891e558edf9991d8fc1
Benjamin Gratch <bgratch@gmail.com>
2019-12-13 11:33:02 -0800

    half of chapter 3

---
+++ automate/sameName3.py
@@ -0,0 +1,13 @@
+def spam():
+    global eggs
+    eggs = 'spam' # this is global
+
+def bacon():
+    eggs = 'bacon' # this is local
+
+def ham():
+    print(eggs) # this is global
+
+eggs = 42 # setting global
+spam()
+print(eggs)
