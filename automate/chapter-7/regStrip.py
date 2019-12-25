8d3ee74b228a342db0680bc6c15e644bfca3d904
Benjamin M. Gratch <bgratch@gmail.com>
2019-12-24 18:56:56 -0800

    Update regStrip.py
    first pass on regStrip.py

--- automate/chapter-7/regStrip.py
+++ automate/chapter-7/regStrip.py
@@ -1,2 +1,11 @@
 #! python3
 # regStrip.py - simulating the strip() command with regex.
+import re
+
+def regStrip(text, mod=r'\w'):
+    stripRegex = re.compile(r'(^' + mod + r'+)|(' + mod + r'+$)')
+    return(stripRegex.sub('xxx', text))
+
+test1 = '   ehyhey!  Lets play tag!   '
+
+print(regStrip(test1))
