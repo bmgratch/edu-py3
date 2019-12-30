64dd5ce57952c4584bd6b59c34d54a25ede7c687
bmgratch <bmgratch@users.noreply.github.com>
2019-12-29 21:35:52 -0800

    finish reg strip py
    Finish practice problems for chapter 7

--- automate/chapter-7/regStrip.py
+++ automate/chapter-7/regStrip.py
@@ -2,9 +2,9 @@
 # regStrip.py - simulating the strip() command with regex.
 import re
 
-def regStrip(text, mod=r'\w'):
+def regStrip(text, mod=r'\s'):
     stripRegex = re.compile(r'(^' + mod + r'+)|(' + mod + r'+$)')
-    return(stripRegex.sub('xxx', text))
+    return(stripRegex.sub('', text))
 
 test1 = '   ehyhey!  Lets play tag!   '
 
