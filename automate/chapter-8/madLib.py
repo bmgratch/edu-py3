7c375650dd2547b90ba6904cbec433849b2edfab
bmgratch <45016798+bmgratch@users.noreply.github.com>
2020-01-02 11:26:07 -0800

    add file logic for completed madlib

--- automate/chapter-8/madLib.py
+++ automate/chapter-8/madLib.py
@@ -5,10 +5,10 @@ import os, sys, re
 
 # Generate mad lib regex
 madLibRegex = re.compile(r'((NOUN)|(ADJECTIVE)|(VERB)|(ADVERB))')
-nounRegex = re.compile(r'\WNOUN')
-adjRegex = re.compile(r'\WADJECTIVE')
-verbRegex = re.compile(r'\WVERB')
-advRegex = re.compile(r'\WADVERB')
+#nounRegex = re.compile(r'\WNOUN')      # THESE were unneeded.
+#adjRegex = re.compile(r'\WADJECTIVE')
+#verbRegex = re.compile(r'\WVERB')
+#advRegex = re.compile(r'\WADVERB')
 
 # Get the data from the file
 if len(sys.argv) != 2:
@@ -22,10 +22,13 @@ else:
 # TODO Regex search the file for CAPS text
 while True:
     libRep = madLibRegex.search(ml)
-    if 
+    #I need a try: exeption: clause here
     print("Enter a " + libRep.group().lower())
     rep = input()
     ml = madLibRegex.sub(rep, ml, 1)
     print(ml)
     
-# TODO Save to new file
+# TODO Save to new file, needs testing
+madLibComplete = open(f'MadLib_new.txt', 'w') #makes the file
+madLibComplete.write(ml)                      # this should be done!
+madLibComplete.close()                      #close file
