e19175b0eed596534661f86904ad817eb1169b04
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-05 17:52:16 -0800

    finish protein_translation.py
    and submitted.

--- exercism/python/protein-translation/protein_translation.py
+++ exercism/python/protein-translation/protein_translation.py
@@ -1,2 +1,37 @@
 def proteins(strand):
-    pass
+    # init definitions
+    protein = []
+    dictRNA = {
+        'AUG': "Methionine",
+        'UUU': "Phenylalanine",
+        'UUC': "Phenylalanine",
+        'UUA': "Leucine",
+        'UUG': "Leucine",
+        'UCU': "Serine",
+        'UCC': 'Serine',
+        'UCA': 'Serine',
+        'UCG': 'Serine',
+        'UAU': 'Tyrosine',
+        'UAC': 'Tyrosine',
+        'UGU': "Cysteine",
+        'UGC': "Cysteine",
+        'UGG': 'Tryptophan'
+        }
+    codon = ''
+    pro = ''
+
+    for p in strand:
+        codon += p
+
+        if len(codon) < 3:
+            pass
+        elif len(codon) == 3:
+            # dictRNA logic
+
+            pro = dictRNA.get(codon, None)
+            if pro != None:
+                protein.append(pro)
+                codon = ''
+            else:
+                break
+    return(protein)
