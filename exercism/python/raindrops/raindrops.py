01951a6428f4c8dd918fa738cd1decfb503b8d91
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-05 16:16:39 -0800

    complete raindrops

--- exercism/python/raindrops/raindrops.py
+++ exercism/python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    drop = ""
+    if number % 3 == 0:
+        drop += 'Pling'
+    if number % 5 == 0:
+        drop += 'Plang'
+    if number % 7 == 0:
+        drop += 'Plong'
+    if drop == "":
+        drop = str(number)
+    return(drop)
