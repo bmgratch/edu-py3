a5bd877a89b123c37f28375790d0d92e5a78fdf3
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-05 19:54:38 -0800

    finish backupToZip.py

--- automate/chapter-9/backupToZip.py
+++ automate/chapter-9/backupToZip.py
@@ -22,8 +22,19 @@ def backupToZip(folder):
     print('Creating %s...' % (zipFilename))
     backupZip = zipfile.ZipFile(zipFilename, 'w')
     
-    # todo walk the entire folde tree and compress the files
+    #walk the entire folde tree and compress the files
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('Adding files in %s...' % (foldername))
+        # add the current folder to the ZIP file.
+        backupZip.write(foldername)
+        # add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue #don't backup ZIP files
+            backupZip.write(os.path.join(foldername, filename))
+        backupZip.close()
 
     print('Done.')
 
-backupToZip('C:\\delicious')
+backupToZip('C:\\progra')
