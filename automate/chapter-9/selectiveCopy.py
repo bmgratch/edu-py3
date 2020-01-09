30e9851b4bb8985c30851e2d533f65cd14fa7e10
bmgratch <bmgratch@users.noreply.github.com>
2020-01-08 17:38:20 -0800

    testing selectiveCopy

--- automate/chapter-9/selectiveCopy.py
+++ automate/chapter-9/selectiveCopy.py
@@ -3,8 +3,22 @@
 # moves them to a new folder
 # selectiveCopy.py [folder to copy] [folder to copy to]
 
-import os, sys
+import os, sys, shutil
 
 ## TODO get arguments 
 ## TODO Walk folder, get list
 ## TODO copy from A to B
+
+print(sys.argv)
+
+if len(sys.argv) != 3:
+    print('Requires 2 arguments:\n selectiveCopy.py <copy folder> <destination folder>')
+
+else:
+    source = sys.argv[1]
+    dest = sys.argv[2]
+    for folder, subfolder, filenames in os.walk(source):
+        print(folder)
+        shutil.copy(folder, dest)
+        print(subfolder)
+        print(filenames)
