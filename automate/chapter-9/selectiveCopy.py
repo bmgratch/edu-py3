fd8ff6de91073444e814c0ff7acc525afed5b141
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-08 16:54:28 -0800

    Update selectiveCopy.py

--- automate/chapter-9/selectiveCopy.py
+++ automate/chapter-9/selectiveCopy.py
@@ -1,4 +1,10 @@
 #! python3
 # selectiveCopy.py - copies every file with a certain extension and
 # moves them to a new folder
+# selectiveCopy.py [folder to copy] [folder to copy to]
 
+import os, sys
+
+## TODO get arguments 
+## TODO Walk folder, get list
+## TODO copy from A to B
