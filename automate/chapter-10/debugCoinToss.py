f114d33f8cf8bfcfab440c6432d9b5603a4eebc0
Benjamin M. Gratch <bgratch@gmail.com>
2020-01-16 14:21:32 -0800

    Update debugCoinToss.py

--- automate/chapter-10/debugCoinToss.py
+++ automate/chapter-10/debugCoinToss.py
@@ -2,13 +2,20 @@ import random
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
-    guess = input()
+    guess = input().lower()
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == 1:
+    toss = 'heads'
+else:
+    toss = 'tails'
+    
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
+    while guess not in ('heads', 'tails'):
+        print('Enter heads or tails:')
+        guess = input().lower()
     if toss == guess:
        print('You got it!')
     else:
