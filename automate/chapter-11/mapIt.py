83b5ff1fd317d061e91d1ad48b8b4c4c1401c1b0
bmgratch <bmgratch@users.noreply.github.com>
2020-01-21 21:27:55 -0800

    Update mapIt.py

--- automate/chapter-11/mapIt.py
+++ automate/chapter-11/mapIt.py
@@ -1,2 +1,14 @@
 #! python3
 # mapIt.py - a web browsing map fucntion
+
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ''.join(sys.argv[1:])
+    
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
