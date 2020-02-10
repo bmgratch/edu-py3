9123a157e04b1bbf65e63bd3ead2d02105ee69de
Benjamin M. Gratch <bgratch@gmail.com>
2020-02-09 16:05:37 -0800

    Update hamming.py

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,9 +1,8 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Strands do not match lengths.')
-    else:
-        hamming = 0
-        for dna in range(0, len(strand_a)):
-            if strand_a[dna] != strand_b[dna]:
-                hamming += 1
-        return hamming
+    hamming = 0
+    for dna in range(0, len(strand_a)):
+        if strand_a[dna] != strand_b[dna]:
+            hamming += 1
+    return hamming
