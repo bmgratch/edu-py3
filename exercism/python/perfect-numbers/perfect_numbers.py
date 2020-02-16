bfdc44438ed232c6926e10874d25c98a151b8476
Benjamin M. Gratch <bgratch@gmail.com>
2020-02-16 12:58:24 -0800

    Update perfect_numbers.py

--- exercism/python/perfect-numbers/perfect_numbers.py
+++ exercism/python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,13 @@
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError('%s is not a natural number.' % number)
+    aliquot = 0
+    for n in range(1, number):
+        if number % n == 0:
+            aliquot += n
+    if aliquot == number:
+        return 'perfect'
+    elif aliquot < number:
+        return 'deficient'
+    else:
+        return 'abundant'
