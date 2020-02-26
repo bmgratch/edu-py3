88c94b1947d5e5fc445c6dfe740cd0033bf1f093
Benjamin M. Gratch <bgratch@gmail.com>
2020-02-25 18:17:09 -0800

    Update blankRowInserter.py

--- automate/chapter-12/blankRowInserter.py
+++ automate/chapter-12/blankRowInserter.py
@@ -4,8 +4,17 @@
 
 import sys, openpyxl
 
+# Get parameters from command line
 if len(sys.argv) == 4:
-    pass
+    startRow = sys.argv[1]
+    blankRow = sys.argv[2]
+    file = sys.argv[3]
 else:
     print("Proper Usage: blankRowInserter.py (row) (blanks) (file.xlsx)")
     sys.exit()
+
+# TODO Open file
+
+# TODO add blanks
+
+# TODO save file
