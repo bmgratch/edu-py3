ff69ea5481e0c0e9fd1a58d0d78e014699561840
Benjamin M. Gratch <bgratch@gmail.com>
2020-02-25 18:07:44 -0800

    Update blankRowInserter.py

--- automate/chapter-12/blankRowInserter.py
+++ automate/chapter-12/blankRowInserter.py
@@ -1,2 +1,11 @@
 #! python3
-# blankRowInserter.py
+# blankRowInserter.py - inserts blank rows into a spreadsheet
+# usage: python blankRowInserter.py <row> <blanks> <file.xlsx>
+
+import sys, openpyxl
+
+if len(sys.argv) == 4:
+    pass
+else:
+    print("Proper Usage: blankRowInserter.py (row) (blanks) (file.xlsx)")
+    sys.exit()
