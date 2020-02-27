2b1b91a6fa864ffea2e81eb22e13a21a3f6fd2be
Benjamin M. Gratch <bgratch@gmail.com>
2020-02-26 18:54:17 -0800

    Update blankRowInserter.py

--- automate/chapter-12/blankRowInserter.py
+++ automate/chapter-12/blankRowInserter.py
@@ -6,12 +6,17 @@ import sys, openpyxl
 
 # Get parameters from command line
 if len(sys.argv) == 4:
-    startRow = sys.argv[1]
-    blankRow = sys.argv[2]
+    startRow = int(sys.argv[1])
+    blankRow = int(sys.argv[2])
     file = sys.argv[3]
 else:
     print("Proper Usage: blankRowInserter.py (row) (blanks) (file.xlsx)")
-    sys.exit()
+    #sys.exit()
+    #for testing
+    startRow = 4
+    blankRow = 2
+    file = 'multiplication-table-10.xlsx'
+    
 
 # Open file
 wb = openpyxl.load_workbook(file)
@@ -21,7 +26,7 @@ sheet2 = wb2.active
 sheet2.title = sheet.title #copy sheet title, why not
 
 # TODO copy rows, add blanks
-for row in range(1, sheet.max_row +1):
+for row in range(1, sheet.max_row):
     if row < startRow:
         #copy to row
         pass
@@ -31,4 +36,4 @@ for row in range(1, sheet.max_row +1):
 
 # Save file as file_new.xlsx
 
-wb2.save('%s_new.xlsx' % file)
+wb2.save('new_%s' % file)
