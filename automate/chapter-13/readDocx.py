9e65d010523c965e640c2704049bb1239ec8c8c5
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-10 17:11:47 -0700

    Update readDocx.py

--- automate/chapter-13/readDocx.py
+++ automate/chapter-13/readDocx.py
@@ -7,5 +7,5 @@ def getText(filename):
     doc = docx.Document(filename)
     fullText = []
     for para in doc.paragraphs:
-        fullText.append(para.text)
-    return '\n'.join(fullText)
+        fullText.append(' ' + para.text)
+    return '\n\n'.join(fullText)
