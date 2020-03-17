94a81363d78ba2d072df1024a28c29e4cef7dc83
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-17 16:10:13 -0700

    Update decryptPdf.py

--- automate/chapter-13/decryptPdf.py
+++ automate/chapter-13/decryptPdf.py
@@ -3,6 +3,7 @@
 
 import PyPDF2, sys
 pwDict = 'dictionary.txt'
+password = None
 
 #get .pdf from command line
 if len(sys.argv) == 2:
@@ -13,6 +14,7 @@ else:
 
 # First load the dictionary of passwords
 pwFile = open(pwDict)
+passwords = pwFile.readlines()
 
 # second, load the PDF
 pdfReader = PyPDF2.PdfFileReader(open(pdfFile), rb)
@@ -21,3 +23,12 @@ if not pdfReader.isEncrypted:
     sys.exit()
 
 #third, loop through and check every word until succeeded.
+for pw in passwords:
+    if pdfReader.decrypt(pw.upper()) == 1:
+        password = pw
+        break
+    if pdfReader.decrypt(pw.lower()) == 1:
+        password = pw
+        break
+if password != None:
+    print(password)
