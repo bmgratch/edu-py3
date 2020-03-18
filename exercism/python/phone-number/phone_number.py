2751bbfbc9e0098d392f052eeea6046978599bd2
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-18 12:06:11 -0700

    Update phone_number.py
    first pass, test against pytest

--- exercism/python/phone-number/phone_number.py
+++ exercism/python/phone-number/phone_number.py
@@ -1,3 +1,10 @@
 class PhoneNumber:
     def __init__(self, number):
-        pass
+        phone = ''
+        for n in number:
+            if n.isnumeric():
+                phone += n
+        if phone[0] == '1':
+            phone = phone[1:]
+        print(phone)
+        self.number = phone
