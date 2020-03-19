037ba5af9d2009742e900db9e7b9885b44398ab4
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-19 15:28:00 -0700

    Update isbn_verifier.py

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,13 @@
 def is_valid(isbn):
-    pass
+    checksum = 0
+    isbn = isbn.replace('-', '')
+    if len(isbn) != 10:
+        return False
+    for d in range(len(isbn)):
+        if d == 9 and isbn[d] == 'X':
+            checksum += 10
+        elif isbn[d].isnumeric():
+            checksum += (10-d) * int(isbn[d])
+        else:
+            return False
+    return (checksum % 11) == 0
