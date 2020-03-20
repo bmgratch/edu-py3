88c32e72d7cff5c570c7c16ae67e1df37f50da47
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-20 12:19:47 -0700

    Update difference_of_squares.py

--- exercism/python/difference-of-squares/difference_of_squares.py
+++ exercism/python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,12 @@
-def square_of_sum(number):
-    pass
+# I could just import the Math module,
+# but I'm trying to do this without importing.
 
+def square_of_sum(number):
+    sum_number = (number * (number + 1)) // 2
+    return sum_number ** 2
 
 def sum_of_squares(number):
-    pass
-
+    return (number * (number + 1) * ((number * 2) + 1)) // 6
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
