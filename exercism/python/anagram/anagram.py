2be13dbab84f059e192c18cd4d620481969d4414
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-20 12:56:43 -0700

    Update anagram.py

--- exercism/python/anagram/anagram.py
+++ exercism/python/anagram/anagram.py
@@ -1,2 +1,7 @@
 def find_anagrams(word, candidates):
-    pass
+    answers = []
+    for word2 in candidates:
+        if sorted(word.lower()) == sorted(word2.lower()) and word.lower() != word2.lower():
+            answers.append(word2)
+    return answers
+
