0e7a18fe71e41f4355a3d875d26d48cdcfb0f6d4
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-24 14:38:08 -0700

    Update triangle.py

--- exercism/python/triangle/triangle.py
+++ exercism/python/triangle/triangle.py
@@ -1,10 +1,26 @@
 def equilateral(sides):
-    pass
+    if valid_triangle(sorted(sides)):
+        return min(sides) == max(sides)
+    else:
+        return False
 
 
 def isosceles(sides):
-    pass
-
+    if valid_triangle(sorted(sides)):
+        return len(set(sides)) < 3
+    else:
+        return False
 
 def scalene(sides):
-    pass
+    if valid_triangle(sorted(sides)):
+        return len(set(sides)) == 3
+    else:
+        return False
+
+def valid_triangle(sides):
+    print(len(sides) != 3)
+    print(sides[-1] > sides[0] + sides[1])
+    print(sides[0] <= 0)
+    if len(sides) != 3 or sides[-1] > (sides[0] + sides[1]) or sides[0] <= 0:
+        return False
+    return True
