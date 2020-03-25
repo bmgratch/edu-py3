dfa8889ad441a3371621262ae09e392beb6287dd
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-25 13:19:38 -0700

    Update grains.py

--- exercism/python/grains/grains.py
+++ exercism/python/grains/grains.py
@@ -1,6 +1,8 @@
 def square(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError('Square must be positive.')
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    return sum(square(n) for n in range(1, 65))
