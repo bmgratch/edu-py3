2c071bf65efe6f8717fbe4d912f61a846543a012
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-26 13:42:17 -0700

    Update saddle_points.py
    finish exercise

--- exercism/python/saddle-points/saddle_points.py
+++ exercism/python/saddle-points/saddle_points.py
@@ -1,4 +1,6 @@
 def saddle_points(matrix):
+    if not valid_matrix(matrix):
+        return []
     saddle, maxes, mins = [], [], []
     columns = [[0]*len(matrix) for x in range(len(matrix[0]))]
     for i, row in enumerate(matrix):
@@ -12,3 +14,12 @@ def saddle_points(matrix):
             if n1 == n2:
                 saddle.append({"row" : x + 1, "column" : y + 1})
     return saddle
+
+def valid_matrix(matrix):
+    if not matrix:
+        return False
+    test_rows = [len(row) for row in matrix]
+    if max(test_rows) != min(test_rows):
+        raise ValueError('Invalid matrix')
+    return True
+    
