7a1b25026491c1c0442f8c27af7f336896a00348
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-26 13:35:24 -0700

    Update saddle_points.py

--- exercism/python/saddle-points/saddle_points.py
+++ exercism/python/saddle-points/saddle_points.py
@@ -1,6 +1,5 @@
 def saddle_points(matrix):
-    saddle = []
-    maxes, mins = [], []
+    saddle, maxes, mins = [], [], []
     columns = [[0]*len(matrix) for x in range(len(matrix[0]))]
     for i, row in enumerate(matrix):
         for i2, c in enumerate(row):
@@ -11,8 +10,5 @@ def saddle_points(matrix):
     for x, n1 in enumerate(maxes):
         for y, n2 in enumerate(mins):
             if n1 == n2:
-                saddle.append((x + 1, y + 1))
+                saddle.append({"row" : x + 1, "column" : y + 1})
     return saddle
-
-#test = [[9, 8, 7], [5, 3, 2], [6, 6, 7]]
-#print(saddle_points(test))
