94cacbf6dd59e6fed10e633f48673f450902a729
Benjamin M. Gratch <bgratch@gmail.com>
2020-03-31 13:48:45 -0700

    Update twelve_days.py
    change to use str.join() instead of concatination

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -24,12 +24,13 @@ def recite(start_verse, end_verse):
             "eleventh",
             "twelfth"]
     song = []
+    verse = ["On the ", None , " day of Christmas my true love gave to me: "]
     for i in range(start_verse - 1, end_verse):
-        verse = "On the %s day of Christmas my true love gave to me: " % xth[i]
+        verse = verse[:3]
+        verse[1] = xth[i]
         for n in range(i, -1, -1):
             if n == 0 and i != 0:
-                verse +="and %s" % gifts[n]
-            else:
-                    verse += gifts[n]
-        song.append(verse)
+                verse.append("and ")
+            verse.append(gifts[n])
+        song.append(''.join(verse))
     return song
