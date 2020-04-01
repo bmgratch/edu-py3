0c743a1ee62e29f4c0aff4bf67e0e3cc287fdab7
Benjamin M. Gratch <bgratch@gmail.com>
2020-04-01 15:42:53 -0700

    Update simple_cipher.py

--- exercism/python/simple-cipher/simple_cipher.py
+++ exercism/python/simple-cipher/simple_cipher.py
@@ -1,32 +1,47 @@
+from random import randint
+
 class Cipher:
     
     def __init__(self, key=None):
         if not key:
-            self.key = 'd'
+            self.key = self.keygen()    # default generation if none provided
         else:
             self.key = key
     
     def encode(self, text):
         newText = []
-        tKey = self.key
+        tKey = self.key # tKey repeats the key if it's shorter than the message
         while len(tKey) < len(text):
             tKey += self.key
         for i, c in enumerate(text):
             if c.islower():
-                newText.append(chr(self.cton(c) + self.cton(tKey[i]) % 26 + 97))
+                newText.append(chr((self.cton(c) + self.cton(tKey[i])) % 26 + 97)                               )
             elif c.isupper():
-                newText.append(chr(ord(c) + self.cton(tKey[i]) % 26 + 96))
+                newText.append(chr((self.cton(c) + self.cton(tKey[i])) % 26 + 65))
             else:
                 newText.append(c)
         return ''.join(newText)
 
     def decode(self, text):
-        pass
+        newText = []
+        tKey = self.key # tKey repeats the key if it's shorter than the message
+        while len(tKey) < len(text):
+            tKey += self.key
+        for i, c in enumerate(text):
+            if c.islower():
+                newText.append(chr((self.cton(c) - self.cton(tKey[i])) % 26 + 97))
+            elif c.isupper():
+                newText.append(chr((self.cton(c) - self.cton(tKey[i])) % 26 + 65))
+            else:
+                newText.append(c)
+        return ''.join(newText)
 
+    # alpha char to number
     def cton(self, char):
+        if char.isupper():
+            return ord(char) - 65
         return ord(char) - 97
-
-ci = Cipher('d')
-print(ci.key)
-print(ci.encode('iamapandabear'))
-print(ci.cton('d'))
+    
+    # generates a random 100-character lowercase key
+    def keygen(self):
+        return ''.join(chr(randint(0, 25) + 97) for i in range(100))
