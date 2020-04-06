e903506ed93b36e768fb7ea9986cceb96b42c3e1
Benjamin M. Gratch <bgratch@gmail.com>
2020-04-06 12:42:11 -0700

    Update twelve_days.py

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,16 +1,16 @@
 def recite(start_verse, end_verse):
-    gifts = ["a Partridge in a Pear Tree.",
-        "two Turtle Doves, ",
-        "three French Hens, ",
-        "four Calling Birds, ",
-        "five Gold Rings, ",
-        "six Geese-a-Laying, ",
-        "seven Swans-a-Swimming, ",
-        "eight Maids-a-Milking, ",
-        "nine Ladies Dancing, ",
-        "ten Lords-a-Leaping, ",
-        "eleven Pipers Piping, ",
-        "twelve Drummers Drumming, "]
+    gifts = ["a Partridge in a Pear Tree",
+        "two Turtle Doves",
+        "three French Hens",
+        "four Calling Birds",
+        "five Gold Rings",
+        "six Geese-a-Laying",
+        "seven Swans-a-Swimming",
+        "eight Maids-a-Milking",
+        "nine Ladies Dancing",
+        "ten Lords-a-Leaping",
+        "eleven Pipers Piping",
+        "twelve Drummers Drumming"]
     xth = [ "first",
             "second",
             "third",
@@ -23,14 +23,11 @@ def recite(start_verse, end_verse):
             "tenth",
             "eleventh",
             "twelfth"]
+    
     song = []
-    verse = ["On the ", None , " day of Christmas my true love gave to me: "]
     for i in range(start_verse - 1, end_verse):
-        verse = verse[:3]
-        verse[1] = xth[i]
-        for n in range(i, -1, -1):
-            if n == 0 and i != 0:
-                verse.append("and ")
-            verse.append(gifts[n])
-        song.append(''.join(verse))
+        verse_gifts = gifts[i::-1]
+        if i != 0:
+            verse_gifts[-1] = "and %s" % verse_gifts[-1]
+        song.append("On the %s day of Christmas my true love gave to me: %s." % (xth[i], (', '.join(verse_gifts))))
     return song
