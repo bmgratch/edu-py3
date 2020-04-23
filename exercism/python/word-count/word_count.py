829f6c8bc7e127af87078cebf389d9a2c03fe503
Benjamin M. Gratch <bgratch@gmail.com>
2020-04-23 10:40:49 -0700

    Update word_count.py
    remove unused function

--- exercism/python/word-count/word_count.py
+++ exercism/python/word-count/word_count.py
@@ -2,13 +2,11 @@ def count_words(sentence):
     counter = {}
     word_list = strip_punctuation(sentence.lower()).split()
     for word in word_list:
-        if len(word) > 0:
+        if word != '':
             counter.setdefault(word.strip("'"), 0)
             counter[word.strip("'")] += 1
     return counter
 
-def word_strip(word):
-    return "".join(c for c in word if c.isalnum())
 
 # just get rid of non-apostrophe punctuation
 def strip_punctuation(sentence):
