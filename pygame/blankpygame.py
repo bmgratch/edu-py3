58a45f58f8f6c5cfd06ec15adfef76a75f5ef72d
Benjamin M. Gratch <bgratch@gmail.com>
2020-04-28 14:22:00 -0700

    begin ch1: pygame
    reviewing

---
+++ pygame/blankpygame.py
@@ -0,0 +1,15 @@
+# First Pygame
+# Hello World
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400,300))
+pygame.display.set_caption('Hello world!')
+while True: # Main Pygame loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
