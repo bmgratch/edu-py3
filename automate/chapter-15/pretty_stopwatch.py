d6010c44d2efb96ab93b15a952e5d4a158608d3e
Benjamin M. Gratch <bgratch@gmail.com>
2020-05-04 14:04:39 -0700

    Update pretty_stopwatch.py
    finish ch 15 first project

--- automate/chapter-15/pretty_stopwatch.py
+++ automate/chapter-15/pretty_stopwatch.py
@@ -1,7 +1,7 @@
 #! python3
 # stopwatch.py - A simple stopwatch program
 
-import time
+import time, pyperclip
 
 # display the program's instructions.
 print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch.  Press Ctrl-C to quit.')
@@ -10,6 +10,7 @@ print('Started!')
 startTime = time.time()     # get the first lap's start time.
 lastTime = startTime
 lapNum = 1
+lapFull = ''
 
 # Start tracking the lap times.
 try:
@@ -18,9 +19,14 @@ try:
         lapTime = round(time.time() - lastTime, 2)
         totalTime = round(time.time() - startTime, 2)
 
-        print(('Lap #%s:' % lapNum).ljust(8), str(totalTime).rjust(5) + ' (', str(lapTime).rjust(4)+ ')', end='')
+        lapText = ('Lap #%s:' % lapNum).ljust(8)+ str(totalTime).rjust(6) + ' (' + str(lapTime).rjust(6)+ ')'
+        print (lapText, end='')
+#        print(('Lap #%s:' % lapNum).ljust(8), str(totalTime).rjust(5) + ' (', str(lapTime).rjust(4)+ ')', end='')
         lapNum += 1
         lastTime = time.time()  # reset the last lap time.
+        lapFull+= lapText + '\n'
 except KeyboardInterrupt:
     # Handle the Ctrl-C exception to keep its error message from displaying.
     print('\nDone.')
+    pyperclip.copy(lapFull)
+    print('Full records transfered to your clipboard!')
