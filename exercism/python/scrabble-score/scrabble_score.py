430d6442c30d26eb00efe47598063541d9663658
Benjamin M. Gratch <bgratch@gmail.com>
2020-06-15 14:10:56 -0700

    Update scrabble_score.py

--- exercism/python/scrabble-score/scrabble_score.py
+++ exercism/python/scrabble-score/scrabble_score.py
@@ -1,3 +1,4 @@
+# Easy to manipulate constant.
 SCORES = {
     ('Q', 'Z') : 10,
     ('J', 'X') : 8,
@@ -7,11 +8,13 @@ SCORES = {
     ('D', 'G') : 2
     }
 
+# Construct the dictionary for easier use.
+letterScore = {}
+for k, v in SCORES.items():
+    for k0 in k:
+        letterScore[k0] = v
+
 def score(word):
-    letterScore = {}
-    for k, v in SCORES.items():
-        for k0 in k:
-            letterScore[k0] = v
     total = 0
     for w in word.upper():
         total+= letterScore.get(w, 1)
