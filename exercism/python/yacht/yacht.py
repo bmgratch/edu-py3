2dbb8eec311ae6b0ec8434fde0c85d98833595f8
Benjamin M. Gratch <bgratch@gmail.com>
2020-06-16 13:12:47 -0700

    finish yacht.py

--- exercism/python/yacht/yacht.py
+++ exercism/python/yacht/yacht.py
@@ -14,17 +14,17 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = lambda d: 50 #FIX ME
+YACHT = lambda d: 50 if d.count(d[0]) == 5 else 0
 ONES = lambda d: d.count(1)
 TWOS = lambda d: d.count(2) * 2
 THREES = lambda d: d.count(3) * 3
 FOURS = lambda d: d.count(4) * 4
 FIVES = lambda d: d.count(5) * 5
 SIXES = lambda d: d.count(6) * 6
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = lambda d: 30 if d.sort() == [1, 2, 3, 4, 5] else 0
-BIG_STRAIGHT = lambda d: 30 if d.sort() == [2, 3, 4, 5, 6] else 0
+FULL_HOUSE = lambda d: sum(d) if len(set(d)) == 2 and (d.count(d[0]) == 2 or d.count(d[0]) == 3) else 0
+FOUR_OF_A_KIND = lambda d: 4 * sorted(set(d))[-1]  if len(set(d)) <= 2 and (d.count(d[0]) <= 1 or d.count(d[0]) >= 4) else 0
+LITTLE_STRAIGHT = lambda d: 30 if sorted(d) == [1, 2, 3, 4, 5] else 0
+BIG_STRAIGHT = lambda d: 30 if sorted(d) == [2, 3, 4, 5, 6] else 0
 CHOICE = lambda d: sum(d)
 
 
