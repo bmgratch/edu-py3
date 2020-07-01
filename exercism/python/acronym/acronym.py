e0af1f8fff1975b60f53593ef0f5a7aeaf0c9d46
Benjamin M. Gratch <bgratch@gmail.com>
2020-07-01 13:18:53 -0700

    submit acronym.py
    First Submission

--- exercism/python/acronym/acronym.py
+++ exercism/python/acronym/acronym.py
@@ -1,8 +1,9 @@
 def abbreviate(words):
+    cleanWords = words.replace('\'', '')
     tla = []
-    for i in range(0, len(words)):
-        if words[i].isalpha() and i == 0:
-            tla.append(words[i].upper())
-        elif i != 0 and words[i].isalpha() and not words[i-1].isalpha():
-            tla.append(words[i].upper())
+    for i in range(0, len(cleanWords)):
+        if cleanWords[i].isalpha() and i == 0:
+            tla.append(cleanWords[i].upper())
+        elif i != 0 and cleanWords[i].isalpha() and not cleanWords[i-1].isalpha():
+            tla.append(cleanWords[i].upper())
     return ''.join(tla)
