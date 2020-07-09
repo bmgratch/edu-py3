d795ebddc2f685c1718b07b43007d22244c5298f
Benjamin M. Gratch <bgratch@gmail.com>
2020-07-09 14:54:52 -0700

    clever app.py learn jinja2 flow control

--- flask/clever/app.py
+++ flask/clever/app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#dummy data
+all_posts = [
+    {
+        "title" : "Post 1",
+        "content" :"This is the content of Post 1",
+        'author': "Benjamin"
+    },
+    {
+        "title" : "Post 2",
+        "content" :"This is the content of Post 2!"
+    }
+]
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name, id):
